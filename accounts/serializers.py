1c54d24b69b02cc49795c32dafa8078b2425d4ec
doniyor <doniyorbek068@gmail.com>
2024-01-26 16:24:52 +0500

    register

---
+++ accounts/serializers.py
@@ -0,0 +1,15 @@
+# serializers.py
+
+from rest_framework import serializers
+
+
+class RegisterSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+    confirmation_code = serializers.CharField(read_only=True)
+
+
+class ConfirmCodeSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    confirm_code = serializers.IntegerField()
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,49 @@
-from rest_framework.views import APIView
+
+
+from rest_framework import generics, status
 from rest_framework.response import Response
+from .serializers import RegisterSerializer, ConfirmCodeSerializer
+from django.core.cache import cache
+from django.core.mail import send_mail
+from rest_framework.generics import GenericAPIView
+import random
 
-from .utls import generate_verification_code, send_verification_code
+
+class RegistrationAPIView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+
+    def generate_confirmation_code(self):
+        return random.randrange(10000, 99999)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        email = serializer.validated_data['email']
+        password = serializer.validated_data['password']
+
+        confirmation_code = self.generate_confirmation_code()
+
+        cache.set(email, confirmation_code, timeout=300)  # 5 minutes timeout
+
+        send_mail(
+            'Registration Confirmation Code',
+            f'Your confirmation code is: {confirmation_code}',
+            'from@example.com',
+            [email],
+            fail_silently=False,
+        )
+
+        return Response({'confirmation_code': confirmation_code}, status=status.HTTP_201_CREATED)
 
 
-class SendVerificationCode(APIView):
-    def post(self, request):
+class ConfirmCodeApiView(GenericAPIView):
+    serializer_class = ConfirmCodeSerializer
+
+    def post(self, request, *args, **kwargs):
         email = request.data.get('email')
+        confirm_code = request.data.get('confirm_code')
 
-        if email:
-            verification_code = generate_verification_code()
-            send_verification_code(email, verification_code)
-            return Response({'message': 'Verification code sent successfully.'})
-        else:
-            return Response({'error': 'Email not provided.'}, status=400)
+        cached_confirm_code = cache.get(email)
+        if confirm_code == cached_confirm_code:
+            return Response({'message': True})
