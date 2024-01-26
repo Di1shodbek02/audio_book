ea8b1bdc8a326349e8648d25e9b6c6c5d2f67a39
Di1shodbek02 <ahmatovdilshodbek@gmail.com>
2024-01-26 16:35:39 +0500

    ecs

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,11 @@
-# from django.contrib.auth.models import AbstractUser
-# from django.db import models
-#
-#
-# class UserData(AbstractUser):
-#     phone_number = models.CharField(max_length=13, unique=True)
-#     avatar = models.FileField(upload_to='pics', blank=True, null=True)
-#     birth_date = models.DateTimeField(auto_now_add=True)
-#
-#     def __str__(self):
-#         return f"{self.first_name} {self.last_name}"
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class UserData(AbstractUser):
+    phone_number = models.CharField(max_length=13, unique=True)
+    avatar = models.FileField(upload_to='pics', blank=True, null=True)
+    birth_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
