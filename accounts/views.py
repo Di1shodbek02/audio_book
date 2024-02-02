cdc4d051a67bc3bcbd465b5ecd0c283a95bb1006
Di1shodbek02 <ahmatovdilshodbek@gmail.com>
2024-02-02 17:52:31 +0500

    create register view

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,6 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.contrib.auth.tokens import default_token_generator
 from django.core.cache import cache
-from django.db.models import Q
 from django.utils.encoding import force_str, force_bytes
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from passlib.context import CryptContext
